925b97abcbd64c68271652e64aa923f95ec4ffb1
s1500729 <dicenull.dev@gmail.com>
2021-02-23 22:28:05 +0900

    プレイヤーリスト以外の情報をカードヘッダにまとめて表示するよう変更

--- src/molecules/PlayerCard.svelte
+++ src/molecules/PlayerCard.svelte
@@ -6,9 +6,9 @@
     export let data: Player;
 </script>
 
-<div class="card " style="width:160px">
-    <div class="d-flex justify-content-start align-items-center ">
-        <div class="pl-2 pr-1 py-auto">
+<div class="container bg-white">
+    <div class="d-flex justify-content-start align-items-center">
+        <div class="pr-1 py-auto">
             <img
                 src="{apiRoot}icons/{data.id}"
                 alt="{data.id} head "
--- src/molecules/ServerCard.svelte
+++ src/molecules/ServerCard.svelte
@@ -9,17 +9,24 @@
 
 <div class="serverStatus">
     <div class="card" style="margin:1rem;">
-        <div class="card-header">
+        <div class="card-header bg-secondary">
             <div
                 class="d-flex justify-content-between align-items-center w-100"
             >
                 <h3>
                     {#if data.online}
-                        <span class="badge badge-success">OK</span>
+                        <span class="badge badge-success">{data.name}</span>
                     {:else}
-                        <span class="badge badge-danger">Stop</span>
+                        <span class="badge badge-danger">{data.name}</span>
                     {/if}
-                    {data.name}
+                </h3>
+                <h3>
+                    {#if data.description !== ""}
+                        <div
+                            contenteditable="true"
+                            bind:innerHTML={minecraftHtml}
+                        />
+                    {/if}
                 </h3>
                 <h2>
                     {data.playerCount}
@@ -33,13 +40,15 @@
         {#if data.description !== ""}
             <div class="card-body bg-light">
                 <blockquote class="blockquote mb-0">
-                    <div
-                        contenteditable="true"
-                        bind:innerHTML={minecraftHtml}
-                    />
-                    {#each data.players as player}
-                        <PlayerCard bind:data={player} />
-                    {/each}
+                    <div class="container">
+                        <div class="column">
+                            {#each data.players as player}
+                                <div class="col py-1">
+                                    <PlayerCard bind:data={player} />
+                                </div>
+                            {/each}
+                        </div>
+                    </div>
                 </blockquote>
             </div>
         {/if}
